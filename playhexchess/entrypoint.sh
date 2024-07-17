8251ba36fe064c086c824cd8e3f52d20bfe2c011
wsnell <aussiesnells@gmail.com>
2024-07-18 09:22:22 +1200

    fix nginx not starting properly on playhexchess

--- playhexchess/entrypoint.sh
+++ playhexchess/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 cd /hexchess/frontend
-./build.sh
+npm install
+npm run build
+
 
 cd /hexchess/server
 cargo run -r &
 
+cd /hexchess/frontend
+node build/index.js &
+
+nginx -g 'daemon off;'
